0818a7225bd165f449e491922a0d2d79acd0d4ed
Ruslan <ruslan353002@gmail.com>
2022-07-08 16:23:21 +0600

    fix: errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
 // local
 import { App } from './pages/App';
 import reportWebVitals from './reportWebVitals';
-import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <Router>
         <App />
       </Router>
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
