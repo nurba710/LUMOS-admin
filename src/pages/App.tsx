7091a57a3b5a048a24085efa3776689eaa439a9f
Kirill <Kirokyr@gmail.com>
2022-07-08 19:28:03 +0600

    fix: delete Input import and Input component in App

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import GlobalStyle from '../globalStyle';
 import AppRouter from '../router/AppRouter';
-import Input from "../components/Input/Input";
 
 export const App = () => {
   return (
   <div className="App">
     <GlobalStyle/>
     <AppRouter/>
-    <Input placeholder='Name' type='text' onChange={()=> console.log(123)} value=''/>
   </div>
   );
 };
