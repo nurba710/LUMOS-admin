e33bd53085db57ec0b0ae3a43442da311a6212f5
Kirill <Kirokyr@gmail.com>
2022-07-08 19:28:03 +0600

    create button component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import GlobalStyle from '../globalStyle';
 import AppRouter from '../router/AppRouter';
+import Button from "../components/Button/Button";
 
 export const App = () => {
   return (
-    <div className="App">
-      <GlobalStyle />
-      <AppRouter />
-    </div>
+  <div className="App">
+    <GlobalStyle/>
+    <AppRouter/>
+    <Button onClick={() => console.log('123')} name='Click'/>
+  </div>
   );
 };
