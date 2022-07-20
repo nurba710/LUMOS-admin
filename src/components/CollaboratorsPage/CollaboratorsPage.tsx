f7ca115c57e323d08b4884ff7010b24e38922b3f
Nurbek <aibek.uulu.nurbek@gmail.com>
2022-07-21 00:50:39 +0600

    dwadawd

--- src/components/CollaboratorsPage/CollaboratorsPage.tsx
+++ src/components/CollaboratorsPage/CollaboratorsPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import CollaboratorsList from './CollaboratorsList/CollaboratorsList'
 
+console.log('dawdwa')
+
 const CollaboratorsPage: React.FC = () => (
 	<>
 		<CollaboratorsList />
