2a9c481d3e1e9e4e8669a61f209d599944053e04
Nurbek <aibek.uulu.nurbek@gmail.com>
2022-07-20 16:09:58 +0600

    feat add eslint

--- src/components/CollaboratorsPage/CollaboratorItem/CollaboratorItem.tsx
+++ src/components/CollaboratorsPage/CollaboratorItem/CollaboratorItem.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import {colleagueType} from '../../../storeToolkit/Types'
-import {ageHelper} from '../../../common/helper/ageHelper'
-import {defaultAvatar} from '../../../common/consts/auth.const'
-import {ItemWrapper, ItemAvatar, Title, Info, Button} from './CollaboratorItemStyle'
+import { colleagueType } from '../../../storeToolkit/Types'
+import { ageHelper } from '../../../common/helper/ageHelper'
+import { defaultAvatar } from '../../../common/consts/auth.const'
+import { ItemWrapper, ItemAvatar, Title, Info, Button } from './CollaboratorItemStyle'
 
 interface collaboratorItemProps {
 	colleague: colleagueType
 }
 
-const CollaboratorItem: React.FC<collaboratorItemProps> = ({colleague}) => {
-	const {avatar, name, age, position} = colleague
-	console.log('awd')
+const CollaboratorItem: React.FC<collaboratorItemProps> = ({ colleague }) => {
+	const { avatar, name, age, position } = colleague
+	console.log('awda')
 	return (
 		<ItemWrapper>
 			<ItemAvatar background={`url(${avatar || defaultAvatar}) 0 0/ cover no-repeat`} />
